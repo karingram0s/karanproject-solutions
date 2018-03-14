87ac8d4aa39fd829597d40a7e32e25062c59833c
karingram0s <karing.ramos@gmail.com>
2018-03-14 14:51:03 +0800

    First commit

---
+++ piNthDigit.py
@@ -0,0 +1,22 @@
+import math
+from decimal import getcontext, Decimal
+
+#####---- checks if input is numerical. loop will break when an integer is entered
+def checkInput(myinput) :
+	while (myinput.isnumeric() == False) :
+		print('Invalid input, must be a number less than 13')
+		myinput = input('Enter number: ')
+	return int(myinput)
+
+#####---- main
+print('This will print the value of pi up to the desired number of decimal places. Limit is up to 12 places.')
+intinput = checkInput(input('Enter number: '))
+
+while (intinput > 12) :
+	print('Number is greater than 12, please try again')
+	intinput = checkInput(input('Enter number: '))
+
+# set number of decimal places	
+getcontext().prec = intinput + 1
+# print value of pi with /intinput/ number of places
+print(Decimal(math.pi)/1)
